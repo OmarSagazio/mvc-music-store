402d3a15308a0d98bfa6dd093adfa5f3817e6ed8
Omar Sagazio <omar.sagazio@gmail.com>
2018-10-16 09:42:07 +0200

    init app

---
+++ MvcMusicStore/MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers {
+    public class StoreController : Controller {
+        // GET: /Store/
+        public string Index() {
+            return "U called Store.Index() method";
+        }
+
+        // GET: /Store/Browse?genre=?Disco
+        public string Browse(string genre = "") {
+            string message = System.Web.HttpUtility.HtmlEncode("Store.Browse, Genre =" + genre);
+            return message;
+        }
+
+        // GET: /Store/Details/5
+        public string Details(int id = 0) {
+            string message = "Store.Details, ID = " + id;
+            return message;
+        }
+    }
+}
